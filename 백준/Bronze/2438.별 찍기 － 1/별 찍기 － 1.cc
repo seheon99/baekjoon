3ad3ccc5dc5ca33be9c6f147fac3a0865fe29756
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2022-01-30 15:01:10 +0900

    [Bronze III] Title: 별 찍기 - 1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438.별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void) {
+  int n;
+
+  std::cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+      std::cout << "*";
+    }
+    if (i != n - 1)
+      std::cout << std::endl;
+  }
+  return (0);
+}
