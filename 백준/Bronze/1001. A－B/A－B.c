39a4d317ce9be335de1d00d1fd327a2e3ab5bd41
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2022-11-29 22:59:02 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void	ft_putnbr(int nbr)
+{
+	char			*numbers;
+	unsigned int	nbr_unsigned;
+
+	numbers = "0123456789";
+	if (nbr < 0)
+		write(1, "-", 1);
+	nbr_unsigned = nbr > 0 ? nbr : -nbr;
+	if (nbr >= 10)
+		ft_putnbr(nbr_unsigned / 10);
+	write(1, numbers + (nbr_unsigned % 10), 1);
+}
+
+int		main(void)
+{
+	char	str[3];
+
+	read(0, str, 3);
+	ft_putnbr((str[0] - '0') - (str[2] - '0'));
+	return (0);
+}
