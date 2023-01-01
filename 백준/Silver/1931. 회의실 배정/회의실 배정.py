5854a688ea709be597c79cf8c82d10e96a40c730
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2023-01-01 22:48:54 +0900

    [Silver I] Title: 회의실 배정, Time: 256 ms, Memory: 58416 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,7 +1,11 @@
-n = int(input())
+from sys import stdin
+
+lines = stdin.readlines()
+
+n = int(lines[0])
 schedules = []
-for _ in range(n):
-    schedules.append(tuple(map(int, input().split())))
+for i in range(1, n + 1):
+    schedules.append(tuple(map(int, lines[i].rstrip().split())))
 
 schedules.sort(key=lambda x: (x[1], x[0]))
 
