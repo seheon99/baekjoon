02cd7e312c08d5c116fdb87beb59c2a10ac2daa9
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2023-01-12 02:41:47 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -4,24 +4,17 @@ int main(void) {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    int operands[50];
-    char operators[50];
+    int number;
+    char opchar;
     int tot;
     std::cin >> tot;
 
-    int length = 0;
-    while (std::cin >> operators[length] >> operands[length]) {
-        length += 1;
-    }
-
     bool plus = true;
-    for (int i = 0; i < length; i++) {
-        if (plus == true && operators[i] == '+') {
-            tot += operands[i];
-        } else {
+    while (std::cin >> opchar >> number) {
+        if (plus && opchar == '-') {
             plus = false;
-            tot -= operands[i];
         }
+        tot += plus ? number : -number;
     }
     std::cout << tot;
     return 0;
