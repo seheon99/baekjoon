ed1782b3491d4de110acd851ce05d983a12b7a9d
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-02-16 18:41:44 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 212 ms, Memory: 23928 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+const int INF = 2'147'483'647;
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int n;
+  std::cin >> n;
+
+  std::vector<int> sequence(n);
+  for (int& number : sequence) {
+    std::cin >> number;
+  }
+
+  std::vector<int> dp;
+  dp.reserve(n);
+  std::vector<int> index(n), trace(n, -1);
+
+  for (int i = 0; i < n; ++i) {
+    int number = sequence[i];
+    int left = 0, right = dp.size();
+
+    while (left < right) {
+      int mid = (left + right) / 2;
+      if (dp[mid] < number) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+
+    if (left == dp.size()) {
+      dp.push_back(number);
+    } else {
+      dp[left] = number;
+    }
+
+    index[left] = i;
+    trace[i] = (left > 0) ? index[left - 1] : -1;
+  }
+
+  std::cout << dp.size() << '\n';
+
+  std::vector<int> lis;
+  for (int i = index[dp.size() - 1]; i != -1; i = trace[i]) {
+    lis.push_back(sequence[i]);
+  }
+
+  std::reverse(lis.begin(), lis.end());
+
+  for (int num : lis) {
+    std::cout << num << ' ';
+  }
+  std::cout << '\n';
+
+  return 0;
+}
